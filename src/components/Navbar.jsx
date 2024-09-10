bb3c67c1f15f0e1be11191661e2577de5d16ec9d
Bishoy Bassem <bishoybassemsamir@hotmail.com>
2024-09-10 11:44:28 +0300

    Adjusted the menu styling for small screens in Navbar.jsx.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,9 +36,9 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-3 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+          <ul className="list-none flex justify-end items-center flex-1 flex-col">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
